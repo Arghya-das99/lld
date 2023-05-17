d32b5042eaac89484a71b28610b06fc86c2c8620
Arghya Das <arghyaboss44@gmail.com>
2023-05-18 03:22:38 +0530

    adding code

---
+++ src/introtooops/Main.java
@@ -0,0 +1,15 @@
+package introtooops;
+
+public class Main {
+    public static void main(String[] args){
+        Student Aman = new Student( "Aman",1);
+        Student Rohit = new Student("Rohit",2);
+
+        Aman.solvesProblem(1);
+        Rohit.solvesProblem(2);
+        Aman.solvesProblem(3);
+        Rohit.solvesProblem(4);
+    }
+
+}
+
